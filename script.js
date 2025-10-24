a879934a0d25ef2e7ffa61b3a121bd64d9e8cb8f
Samuel Ibañez <zaratesamu99@gmail.com>
2025-10-23 22:02:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -31,14 +31,14 @@ const productos = [
   { 
     id: 5, 
     nombre: 'Buzo Oversize “Storm”', 
-    categoria: 'Busos oversize', 
+    categoria: 'Buzos oversize', 
     precio: 95000, 
     imagen: './img/buzo_storm.png' 
   },
   { 
     id: 6, 
     nombre: 'Buzo Oversize “Skyline”', 
-    categoria: 'Busos oversize', 
+    categoria: 'Buzos oversize', 
     precio: 99000, 
     imagen: './img/buzo2.png' 
   }
